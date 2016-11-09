51e983696663f457dc2b2d734627675df4b48b6a
Takaya Saito <takaya.saito@outlook.com>
2016-11-09 01:39:12 +0100

    Add TestSeed class

---
+++ src/test/lib/mikan/include/test_seed.hpp
@@ -0,0 +1,24 @@
+#include<string>
+#include "gtest/gtest.h"
+#include "get_data_path.hpp"
+#include "mikan_utils.hpp"
+#include "mr3_core.hpp"
+
+template <class TSeedSeqs>
+class TestSeed : public TestIOMR3AS
+{
+protected:
+
+    void test_seed(const char *rnastr, int idx, const char *seq_type) {
+        seqan::RnaString seedseq = rnastr;
+
+        reverseComplement(seedseq);
+        comp_two_rnas(mSeedSeqs.get_seed_seq(idx), seedseq);
+        EXPECT_STREQ(seq_type, seqan::toCString((seqan::CharString)mSeedSeqs.get_seed_type(idx)));
+    }
+
+    TSeedSeqs mSeedSeqs;
+    seqan::StringSet<seqan::CharString> mSeedDef;
+};
+
+typedef TestSeed<mr3as::MR3SeedSeqs<seqan::RnaString> > TestSeedMR3AS;
--- src/test/mk_miranda/seed_m001.cpp
+++ src/test/mk_miranda/seed_m001.cpp
@@ -3,10 +3,11 @@
 #include "gtest/gtest.h"
 #include "test_io.hpp"
 #include "mr3_core.hpp"
+#include "test_seed.hpp"
 
 namespace {
 
-    class SDM001 : public TestIOMR3AS
+    class SDM001 : public TestSeedMR3AS
     {
     protected:
         SDM001() {
@@ -17,6 +18,14 @@ namespace {
             O2FNAME1 = (char *)"test_output2_site_1.txt";
             O2FNAME2 = (char *)"test_output2_mrna_1.txt";
             OMPATH = (char *)"mk_miranda/";
+
+            resize(mSeedDef, 6);
+            mSeedDef[0] = 'Y';
+            mSeedDef[1] = 'Y';
+            mSeedDef[2] = 'Y';
+            mSeedDef[3] = "0";
+            mSeedDef[4] = "0:0";
+            mSeedDef[5] = "0";
         }
     };
 
@@ -27,91 +36,95 @@ namespace {
 
         mSeedSeqs.set_mirna_seq(mirna_seqs[0]);
 
-        resize(mSeedDef, 6);
-        mSeedDef[0] = 'Y';
         mSeedDef[1] = 'N';
         mSeedDef[2] = 'N';
-        mSeedDef[3] = "0";
-        mSeedDef[4] = "0:0";
-        mSeedDef[5] = "0";
 
         int n = mSeedSeqs.create_seed_seqs(mSeedDef);
         EXPECT_EQ(0, n);
-
         EXPECT_EQ(1u, length(mSeedSeqs.mEffectiveSeeds));
 
-        seqan::RnaString rnastr = "AAGGCA";
-        reverseComplement(rnastr);
+        test_seed("AAGGCA", 0, "6mer");
+    }
 
-        comp_two_rnas(mSeedSeqs.get_seed_seq(0), rnastr);
+    TEST_F(SDM001, get_seed_2_1) {
+        read_files(false);
+        mirna_seqs = coreInput.get_mirna_seqs();
+
+        mSeedDef[1] = 'N';
+        mSeedDef[2] = 'N';
+
+        mSeedSeqs.set_mirna_seq(mirna_seqs[0]);
+
+        int n = mSeedSeqs.create_seed_seqs(mSeedDef);
+        EXPECT_EQ(0, n);
+        EXPECT_EQ(1u, length(mSeedSeqs.mEffectiveSeeds));
+
+        test_seed("AAGGCA", 0, "6mer");
+    }
+
+    TEST_F(SDM001, get_seed_2_2) {
+        read_files(false);
+        mirna_seqs = coreInput.get_mirna_seqs();
+
+        mSeedDef[1] = 'N';
+        mSeedDef[2] = 'N';
+
+        mSeedSeqs.set_mirna_seq(mirna_seqs[1]);
+
+        int n = mSeedSeqs.create_seed_seqs(mSeedDef);
+        EXPECT_EQ(0, n);
+        EXPECT_EQ(1u, length(mSeedSeqs.mEffectiveSeeds));
+
+        test_seed("GGAAUG", 0, "6mer");
     }
 
     TEST_F(SDM001, get_seed_6mer) {
         read_files(false);
-
         mirna_seqs = coreInput.get_mirna_seqs();
+
+        mSeedDef[1] = 'N';
+        mSeedDef[2] = 'N';
 
         mSeedSeqs.set_mirna_seq(mirna_seqs[0]);
 
-        resize(mSeedDef, 6);
-        mSeedDef[0] = 'Y';
-        mSeedDef[1] = 'N';
-        mSeedDef[2] = 'N';
-        mSeedDef[3] = "0";
-        mSeedDef[4] = "0:0";
-        mSeedDef[5] = "0";
-
         int n = mSeedSeqs.create_seed_seqs(mSeedDef);
         EXPECT_EQ(0, n);
-
         EXPECT_EQ(1u, length(mSeedSeqs.mEffectiveSeeds));
 
-        seqan::RnaString rnastr = "AAGGCA";
-        reverseComplement(rnastr);
-
-        comp_two_rnas(mSeedSeqs.get_seed_seq(0), rnastr);
-        EXPECT_TRUE(mSeedSeqs.mEffectiveSeeds[0]);
-
-        const char *seq_type = "6mer";
-        EXPECT_STREQ(seq_type, seqan::toCString((seqan::CharString)mSeedSeqs.get_seed_type(0)));
+        test_seed("AAGGCA", 0, "6mer");
     }
 
     TEST_F(SDM001, get_seed_gut) {
         read_files(false);
-
         mirna_seqs = coreInput.get_mirna_seqs();
+
+        mSeedDef[3] = "1";
 
         mSeedSeqs.set_mirna_seq(mirna_seqs[0]);
 
-        resize(mSeedDef, 6);
-        mSeedDef[0] = 'Y';
-        mSeedDef[1] = 'Y';
-        mSeedDef[2] = 'Y';
+        int n = mSeedSeqs.create_seed_seqs(mSeedDef);
+        EXPECT_EQ(0, n);
+        EXPECT_EQ(3u, length(mSeedSeqs.mEffectiveSeeds));
+
+        test_seed("AAGGCA", 0, "6mer");
+        test_seed("AAGACA", 1, "GUT");
+        test_seed("AAAGCA", 2, "GUT");
+    }
+
+    TEST_F(SDM001, get_seed_gum) {
+        read_files(false);
+        mirna_seqs = coreInput.get_mirna_seqs();
+
         mSeedDef[3] = "1";
-        mSeedDef[4] = "0:0";
-        mSeedDef[5] = "0";
+
+        mSeedSeqs.set_mirna_seq(mirna_seqs[0]);
 
         int n = mSeedSeqs.create_seed_seqs(mSeedDef);
         EXPECT_EQ(0, n);
-
         EXPECT_EQ(3u, length(mSeedSeqs.mEffectiveSeeds));
 
-        seqan::RnaString rnastr = "AAGGCA";
-        reverseComplement(rnastr);
-        comp_two_rnas(mSeedSeqs.get_seed_seq(0), rnastr);
-        const char *seq_type1 = "6mer";
-        EXPECT_STREQ(seq_type1, seqan::toCString((seqan::CharString)mSeedSeqs.get_seed_type(0)));
-
-        rnastr = "AAGACA";
-        reverseComplement(rnastr);
-        comp_two_rnas(mSeedSeqs.get_seed_seq(1), rnastr);
-        const char *seq_type2 = "GUT";
-        EXPECT_STREQ(seq_type2, seqan::toCString((seqan::CharString)mSeedSeqs.get_seed_type(1)));
-
-        rnastr = "AAAGCA";
-        reverseComplement(rnastr);
-        comp_two_rnas(mSeedSeqs.get_seed_seq(2), rnastr);
-        const char *seq_type3 = "GUT";
-        EXPECT_STREQ(seq_type3, seqan::toCString((seqan::CharString)mSeedSeqs.get_seed_type(2)));
+        test_seed("AAGGCA", 0, "6mer");
+        test_seed("AAGACA", 1, "GUT");
+        test_seed("AAAGCA", 2, "GUT");
     }
 }
