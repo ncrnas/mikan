5907d5f6a08dbedce7d43d650d403901bea745c2
Takaya Saito <takaya.saito@outlook.com>
2016-12-09 13:30:17 +0100

    Add unit tests for TSSVM seed sites

---
+++ src/test/mk_tssvm/site_02_gu_1.cpp
@@ -0,0 +1,45 @@
+#include<string>
+#include <seqan/sequence.h>
+#include "gtest/gtest.h"
+#include "test_tssvm.hpp"
+
+namespace {
+
+    class Site02GU1 : public TestSiteTSSVM
+    {
+    protected:
+        Site02GU1() {
+            IFNAME1 = (char *)"mir_001.fasta";
+            IFNAME2 = (char *)"ts_02_gu_1.fasta";
+            O1FNAME1 = (char *)"test_output1_site_1.txt";
+            O1FNAME2 = (char *)"test_output1_mrna_1.txt";
+            O2FNAME1 = (char *)"test_output2_site_1.txt";
+            O2FNAME2 = (char *)"test_output2_mrna_1.txt";
+            OMPATH = (char *)"mk_tssvm/";
+        }
+
+        typedef tssvm::TSSVMCore<tssvm::TRNATYPE>::TIndexQGram TIdx;
+        typedef tssvm::TSSVMCore<tssvm::TRNATYPE>::TFinder TFin;
+        typedef tssvm::TSSVMSeedSites<tssvm::TRNATYPE> TSit;
+
+    };
+
+    TEST_F(Site02GU1, mir124_gu) {
+        read_files(false);
+        set_seqs();
+        TIdx index(mrna_seqs);
+        TFin finder(index);
+        TSit sites(index, finder, mrna_seqs);
+
+        int ret_val = sites.find_seed_sites(mirna_seqs[0]);
+        EXPECT_EQ(0, ret_val);
+        EXPECT_EQ(5u, sites.get_length());
+
+        test_sites(sites, 0, "7mer-m8", 0, 24, true, 0);
+
+        test_sites(sites, 1, "GUM", 8, 24, true, 3);
+        test_sites(sites, 2, "GUM", 10, 24, true, 3);
+        test_sites(sites, 3, "GUM", 3, 24, true, 4);
+        test_sites(sites, 4, "GUM", 5, 24, true, 4);
+    }
+}
