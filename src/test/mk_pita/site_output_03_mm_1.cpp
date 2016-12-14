27be54f0c62d3563a41d61ea7f44f1fa4a7939f2
Takaya Saito <takaya.saito@outlook.com>
2016-12-14 07:10:11 +0100

    Add comp orig & mk unit tests for pita

---
+++ src/test/mk_pita/site_output_03_mm_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "test_pita.hpp"
+
+namespace {
+
+    class SiteOut03MM1 : public TestIOPITA
+    {
+    protected:
+        SiteOut03MM1() {
+            IFNAME1 = (char *)"mir_003.fasta";
+            IFNAME2 = (char *)"ts_03_mm_1.fasta";
+            O1FNAME1 = (char *)"so03_mm_1_site_orig.txt";
+            O1FNAME2 = (char *)"so03_mm_1_mrna_orig.txt";
+            O2FNAME1 = (char *)"so03_mm_1_site_mk.txt";
+            O2FNAME2 = (char *)"so03_mm_1_mrna_mk.txt";
+            OMPATH = (char *)"mk_pita/";
+        }
+    };
+
+    TEST_F(SiteOut03MM1, comp_orig_mk) {
+        (void)ptddg::PITACoreMain(argc, (const char **)argv);
+        gtest_compare_two_files(o1file1, o2file1);
+        gtest_compare_two_files(o1file2, o2file2);
+    }
+}
