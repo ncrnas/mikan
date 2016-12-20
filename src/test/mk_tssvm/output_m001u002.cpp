b7a661de3b1a7968a7fec04551210215f54563d4
Takaya Saito <takaya.saito@outlook.com>
2016-12-20 08:12:18 +0100

    Add unit tests of long 3utrs for tssvm

---
+++ src/test/mk_tssvm/output_m001u002.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "test_tssvm.hpp"
+
+namespace {
+
+    class OM001U002 : public TestIOTSSVM
+    {
+    protected:
+        OM001U002() {
+            IFNAME1 = (char *)"mir_001.fasta";
+            IFNAME2 = (char *)"utr3_002.fasta";
+            O1FNAME1 = (char *)"test_output1_site_2.txt";
+            O1FNAME2 = (char *)"test_output1_mrna_2.txt";
+            O2FNAME1 = (char *)"test_output2_site_2.txt";
+            O2FNAME2 = (char *)"test_output2_mrna_2.txt";
+            OMPATH = (char *)"mk_tssvm/";
+        }
+    };
+
+    TEST_F(OM001U002, comp_site) {
+        (void)tssvm::TSSVMCoreMain(argc, (const char **)argv);
+        gtest_compare_two_files(o1file1, o2file1);
+    }
+
+    TEST_F(OM001U002, comp_mrna) {
+        (void)tssvm::TSSVMCoreMain(argc, (const char **)argv);
+        gtest_compare_two_files(o1file2, o2file2);
+    }
+}
