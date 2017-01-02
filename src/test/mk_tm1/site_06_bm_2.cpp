58363506240f971883b1ffa12eb9fedefad7f4f1
Takaya Saito <takaya.saito@outlook.com>
2017-01-02 03:27:35 +0100

    Add bm site unit tests for targetminer

---
+++ src/test/mk_tm1/site_06_bm_2.cpp
@@ -0,0 +1,42 @@
+#include<string>
+#include <seqan/sequence.h>
+#include "gtest/gtest.h"
+#include "test_tm1.hpp"
+
+namespace {
+
+    class Site06BM2 : public TestSiteTM1
+    {
+    protected:
+        Site06BM2() {
+            IFNAME1 = (char *)"mir_001.fasta";
+            IFNAME2 = (char *)"ts_06_bm_2.fasta";
+            O1FNAME1 = (char *)"test_output1_site_1.txt";
+            O1FNAME2 = (char *)"test_output1_mrna_1.txt";
+            O2FNAME1 = (char *)"test_output2_site_1.txt";
+            O2FNAME2 = (char *)"test_output2_mrna_1.txt";
+            OMPATH = (char *)"mk_tm1/";
+        }
+
+        typedef tm1p::TM1Core<tm1p::TRNATYPE>::TIndexQGram TIdx;
+        typedef tm1p::TM1Core<tm1p::TRNATYPE>::TFinder TFin;
+        typedef tm1p::TM1SeedSites<tm1p::TRNATYPE> TSit;
+
+    };
+
+    TEST_F(Site06BM2, mir1_bm) {
+        read_files(false);
+        set_seqs();
+        TIdx index(mrna_seqs);
+        TFin finder(index);
+        TSit sites(index, finder, mrna_seqs);
+
+        int ret_val = sites.find_seed_sites(mirna_seqs[1]);
+        EXPECT_EQ(0, ret_val);
+        EXPECT_EQ(2u, sites.get_length());
+
+        test_sites2(sites, 0, "6mer", 7, 26, true);
+        test_sites2(sites, 1, "6mer", 6, 26, true);
+    }
+
+}
---
+++ src/test/mk_tm1/site_output_06_bm_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "test_tm1.hpp"
+
+namespace {
+
+    class SiteOut06BM1 : public TestIOTM1
+    {
+    protected:
+        SiteOut06BM1() {
+            IFNAME1 = (char *)"mir_003.fasta";
+            IFNAME2 = (char *)"ts_06_bm_1.fasta";
+            O1FNAME1 = (char *)"so06_bm_1_site_orig.txt";
+            O1FNAME2 = (char *)"so06_bm_1_mrna_orig.txt";
+            O2FNAME1 = (char *)"so06_bm_1_site_mk.txt";
+            O2FNAME2 = (char *)"so06_bm_1_mrna_mk.txt";
+            OMPATH = (char *)"mk_tm1/";
+        }
+    };
+
+    TEST_F(SiteOut06BM1, comp_orig_mk) {
+        (void)tm1p::TM1CoreMain(argc, (const char **)argv);
+        gtest_compare_two_files(o1file1, o2file1);
+        gtest_compare_two_files(o1file2, o2file2);
+    }
+}
