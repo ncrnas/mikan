23792332274e09e15daecca3214976fcce938b99
Takaya Saito <takaya.saito@outlook.com>
2017-08-07 21:53:18 +0200

    Add simple file comparion gtest for tm1

--- src/test/lib/mikan/include/mikan_utils.hpp
+++ src/test/lib/mikan/include/mikan_utils.hpp
@@ -8,12 +8,10 @@ int gtest_compare_two_files(seqan::CharString const &f1, seqan::CharString const
 int gtest_compare_two_files2(seqan::CharString const &f1, seqan::CharString const &f2,
                              unsigned score_fld, unsigned round_dec, int ubound);
 
-void comp_scores(std::string &score1, std::string &score2, unsigned round_dec, int ubound);
-
-int gtest_read_file(seqan::CharString const &fname, std::vector<std::string> &lines);
+int gtest_compare_two_files3(seqan::CharString const &f1, seqan::CharString const &f2,
+                             bool uppercase, bool replace_space);
 
 void comp_two_rnas(seqan::RnaString const &rnaseq1, seqan::RnaString const &rnaseq2);
 
-void split_line(std::string &line, std::vector<std::string> &flds);
 
 #endif /* MIKANTEST_UTILS_HPP_ */
--- src/test/lib/mikan/mikan_utils.cpp
+++ src/test/lib/mikan/mikan_utils.cpp
@@ -4,28 +4,24 @@
 #include <seqan/seq_io.h>
 #include "mikan_utils.hpp"
 
+// Static functions
+static int gtest_read_files(seqan::CharString const &fname1, seqan::CharString const &fname2,
+                            std::vector<std::string> &lines1, std::vector<std::string> &lines2);
+
+static int gtest_read_file(seqan::CharString const &fname, std::vector<std::string> &lines);
+
+static void comp_scores(std::string &score1, std::string &score2, unsigned round_dec, int ubound);
+
+static void split_line(std::string &line, std::vector<std::string> &flds);
+
 int gtest_compare_two_files(seqan::CharString const &f1, seqan::CharString const &f2) {
     std::vector<std::string> lines1;
     std::vector<std::string> lines2;
-    std::list<std::string>::iterator it;
 
-    int fres1, fres2;
-
-    fres1 = gtest_read_file(f1, lines1);
-    if (fres1 != 0) {
-        std::cerr << "ERROR: Could not open file1" << std::endl;
+    int res = gtest_read_files(f1, f2, lines1, lines2);
+    if (res != 0) {
         return 1;
     }
-    std::sort(lines1.begin(), lines1.end());
-
-    fres2 = gtest_read_file(f2, lines2);
-    if (fres2 != 0) {
-        std::cerr << "ERROR: Could not open file2" << std::endl;
-        return 1;
-    }
-    std::sort(lines2.begin(), lines2.end());
-
-    EXPECT_EQ(lines1.size(), lines2.size());
 
     for (unsigned i = 0; i < lines1.size(); ++i) {
         EXPECT_STREQ(lines1[i].c_str(), lines2[i].c_str());
@@ -43,25 +39,11 @@ int gtest_compare_two_files2(
 
     std::vector<std::string> lines1;
     std::vector<std::string> lines2;
-    std::list<std::string>::iterator it;
 
-    int fres1, fres2;
-
-    fres1 = gtest_read_file(f1, lines1);
-    if (fres1 != 0) {
-        std::cerr << "ERROR: Could not open file1" << std::endl;
+    int res = gtest_read_files(f1, f2, lines1, lines2);
+    if (res != 0) {
         return 1;
     }
-    std::sort(lines1.begin(), lines1.end());
-
-    fres2 = gtest_read_file(f2, lines2);
-    if (fres2 != 0) {
-        std::cerr << "ERROR: Could not open file2" << std::endl;
-        return 1;
-    }
-    std::sort(lines2.begin(), lines2.end());
-
-    EXPECT_EQ(lines1.size(), lines2.size());
 
     for (unsigned i = 0; i < lines1.size(); ++i) {
         std::vector<std::string> flds1;
@@ -87,26 +69,79 @@ int gtest_compare_two_files2(
     return 0;
 }
 
-void comp_scores(std::string &score1, std::string &score2, unsigned round_dec, int ubound) {
+int gtest_compare_two_files3(
+        seqan::CharString const &f1,
+        seqan::CharString const &f2,
+        bool uppercase,
+        bool replace_space) {
 
-    double ds1 = ::atof(score1.c_str());
-    double ds2 = ::atof(score2.c_str());
-    int s1 = static_cast<int>(ds1 * round_dec);
-    int s2 = static_cast<int>(ds2 * round_dec);
+    std::vector<std::string> lines1;
+    std::vector<std::string> lines2;
 
-    int diff  = s1 - s2;
-    int lbound = -1 * ubound;
+    int res = gtest_read_files(f1, f2, lines1, lines2);
+    if (res != 0) {
+        return 1;
+    }
 
-//    std::cout << s1 << " == " << s2  << std::endl;
-//    std::cout << lbound << " <= " << diff << " <= " << ubound << std::endl;
-//    EXPECT_EQ(s1, s2);
+    for (unsigned i = 0; i < lines1.size(); ++i) {
+        std::vector<std::string> flds1;
+        std::vector<std::string> flds2;
+        split_line(lines1[i], flds1);
+        split_line(lines2[i], flds2);
 
-    EXPECT_TRUE(lbound <= diff && diff <= ubound);
+        for (unsigned j = 0; j < 2; ++j) {
+            if (uppercase) {
+                std::transform(flds1[j].begin(), flds1[j].end(), flds1[j].begin(), toupper);
+                std::transform(flds2[j].begin(), flds2[j].end(), flds2[j].begin(), toupper);
+            }
 
+            if (replace_space) {
+                std::replace(flds1[j].begin(), flds1[j].end(), ' ', '_');
+                std::replace(flds2[j].begin(), flds2[j].end(), ' ', '_');
+            }
+
+            EXPECT_STREQ(flds1[j].c_str(), flds2[j].c_str());
+        }
+
+    }
+
+    return 0;
+}
+
+void comp_two_rnas(seqan::RnaString const &rnaseq1, seqan::RnaString const &rnaseq2) {
+    EXPECT_STREQ(seqan::toCString((seqan::CharString) rnaseq1),
+                 seqan::toCString((seqan::CharString) rnaseq2));
+}
+
+static int gtest_read_files(seqan::CharString const &fname1,
+                     seqan::CharString const &fname2,
+                     std::vector<std::string> &lines1,
+                     std::vector<std::string> &lines2) {
+
+    int fres1, fres2;
+
+    fres1 = gtest_read_file(fname1, lines1);
+    if (fres1 != 0) {
+        std::cerr << "ERROR: Could not open file1" << std::endl;
+        return 1;
+    }
+
+    fres2 = gtest_read_file(fname2, lines2);
+    if (fres2 != 0) {
+        std::cerr << "ERROR: Could not open file2" << std::endl;
+        return 1;
+    }
+
+    std::sort(lines1.begin(), lines1.end());
+    std::sort(lines2.begin(), lines2.end());
+
+    EXPECT_EQ(lines1.size(), lines2.size());
+
+    return 0;
 }
 
 
-int gtest_read_file(seqan::CharString const &fname,
+static int gtest_read_file(seqan::CharString const &fname,
                     std::vector<std::string> &lines) {
 
     std::ifstream file(seqan::toCString(fname));
@@ -119,7 +154,7 @@ int gtest_read_file(seqan::CharString const &fname,
     return 0;
 }
 
-void split_line(std::string &line, std::vector<std::string> &flds) {
+static void split_line(std::string &line, std::vector<std::string> &flds) {
     char *pline = const_cast<char *>(line.c_str());
     char *pch;
 
@@ -137,7 +172,20 @@ void split_line(std::string &line, std::vector<std::string> &flds) {
 
 }
 
-void comp_two_rnas(seqan::RnaString const &rnaseq1, seqan::RnaString const &rnaseq2) {
-    EXPECT_STREQ(seqan::toCString((seqan::CharString) rnaseq1),
-                 seqan::toCString((seqan::CharString) rnaseq2));
+static void comp_scores(std::string &score1, std::string &score2, unsigned round_dec, int ubound) {
+
+    double ds1 = ::atof(score1.c_str());
+    double ds2 = ::atof(score2.c_str());
+    int s1 = static_cast<int>(ds1 * round_dec);
+    int s2 = static_cast<int>(ds2 * round_dec);
+
+    int diff  = s1 - s2;
+    int lbound = -1 * ubound;
+
+//    std::cout << s1 << " == " << s2  << std::endl;
+//    std::cout << lbound << " <= " << diff << " <= " << ubound << std::endl;
+//    EXPECT_EQ(s1, s2);
+
+    EXPECT_TRUE(lbound <= diff && diff <= ubound);
+
 }
