b714981d8d61466c604ab0587908855e6f9576cf
Takaya Saito <takaya.saito@outlook.com>
2017-08-07 05:15:02 +0200

    Add sort and round functions for file comparison

--- src/test/lib/mikan/include/mikan_utils.hpp
+++ src/test/lib/mikan/include/mikan_utils.hpp
@@ -5,8 +5,15 @@
 
 int gtest_compare_two_files(seqan::CharString const &f1, seqan::CharString const &f2);
 
-int gtest_read_file(seqan::CharString const &fname, seqan::StringSet<seqan::CharString> &lines);
+int gtest_compare_two_files2(seqan::CharString const &f1, seqan::CharString const &f2,
+                             unsigned score_fld, unsigned round_dec, int ubound);
+
+void comp_scores(std::string &score1, std::string &score2, unsigned round_dec, int ubound);
+
+int gtest_read_file(seqan::CharString const &fname, std::vector<std::string> &lines);
 
 void comp_two_rnas(seqan::RnaString const &rnaseq1, seqan::RnaString const &rnaseq2);
+
+void split_line(std::string &line, std::vector<std::string> &flds);
 
 #endif /* MIKANTEST_UTILS_HPP_ */
