fc278b2782eba3c62ef8802a4bfd26dec08eafc0
Takaya Saito <takaya.saito@outlook.com>
2017-08-09 19:32:44 +0200

    Move PitaAlign to a new module from pita_site_score

--- src/mikan/lib/pita_ddg/include/pita_site_score.hpp
+++ src/mikan/lib/pita_ddg/include/pita_site_score.hpp
@@ -10,36 +10,10 @@
 #include "mk_rna_sites.hpp"       // MKRMAWithSites
 #include "pita_option.hpp"        // PITAOptions
 #include "pita_seed_site.hpp"     // PITASeedSites
+#include "pita_align.hpp"         // PITAAlign
 #include "vr16_ddg_core.hpp"      // VR16DDGWorkSpace
 
 namespace ptddg {
-
-//
-// Store alignments
-//
-class PITAAlign {
-public:
-    // Define variables
-    seqan::String<bool> mEffectiveSites;
-    seqan::StringSet<seqan::CharString> mAlignMRNA;
-    seqan::StringSet<seqan::CharString> mAlignBars;
-    seqan::StringSet<seqan::CharString> mAlignMiRNA;
-
-public:
-    // Define methods
-    explicit PITAAlign(vr16::VR16DDGWorkSpace &pVRws) : mVRws(pVRws) {}
-
-    // Method prototype
-    void clear_align();
-
-    void resize_align(unsigned pSize);
-
-    void create_align(int pId, mikan::TRNAStr const &pMiRNASeq, mikan::TRNAStr const &pMRNASeq,
-                      seqan::CharString const &pSeedType, unsigned pSitePos, int pMismatchPos);
-
-private:
-    vr16::VR16DDGWorkSpace &mVRws;
-};
 
 //
 // Store dGduplex scores
